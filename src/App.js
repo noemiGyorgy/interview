8ed9e0a204f83cf6498a0de68f10bfd13ff824a7
noemi <gynoemi33@gmail.com>
2020-11-27 15:51:13 +0100

    Upload base code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 function App() {
+  const [birthDate, setBirthDate] = useState(new Date());
+  const [registrationDate, setRegistrationDate] = useState(new Date());
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [taxNumber, setTaxNumber] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Name:
+        <input
+          type="text"
+          onChange={(event) => setName(event.target.value)}
+        ></input>
+      </p>
+      <p>
+        Birth Date:
+        <DatePicker
+          selected={birthDate}
+          onChange={(date) => setBirthDate(date)}
+        />
+      </p>
+      <p>
+        E-mail:
+        <input type="text" onChange={(event) => setEmail(event.target.value)} />
+      </p>
+      <p>
+        Phone:
+        <input type="text" onChange={(event) => setPhone(event.target.value)} />
+      </p>
+      <p>
+        Tax number:
+        <input
+          type="text"
+          onChange={(event) => setTaxNumber(event.target.value)}
+        />
+      </p>
+      <p>
+        Registration time:
+        <DatePicker
+          selected={registrationDate}
+          onChange={(date) => setRegistrationDate(date)}
+        />
+      </p>
+      <p>
+        <input type="submit" />
+      </p>
+      <p>{birthDate.toString()}</p>
     </div>
   );
 }
